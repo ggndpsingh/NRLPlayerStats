b617deec6e5eae4c72900ac4113181264d64d86f
Gagandeep Singh <gagandeep.singh@safetyculture.com>
2019-04-13 10:43:22 +1000

    Add base API protocols (#1)

---
+++ NRLPlayerStats/API/APIRequestBuilder.swift
@@ -0,0 +1,28 @@
+//
+//  APIRequestBuilder.swift
+//  NRLPlayerStats
+//
+//  Created by Gagandeep Singh on 25/2/19.
+//  Copyright © 2019 Gagandeep Singh. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+/// Simple type that implements `URLRequestConvertible`
+/// and is passed to the Alamofire request methods.
+/// This is the bit that adds `API` to `Endpoint` and returns a `URLRequest`
+struct APIRequestBuilder: URLRequestConvertible {
+    let api: API
+    let endpoint: Endpoint
+    
+    func asURLRequest() throws -> URLRequest {
+        guard let url = api.urlComponents(for: endpoint).url else {
+            throw APIError.invalidRequest(builder: self)
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = endpoint.method.rawValue
+        return try endpoint.configure(request)
+    }
+}
