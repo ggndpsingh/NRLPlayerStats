1fbe32ec03a63e0ce7e2e4aa11f5bf98a40a6cf5
Gagandeep Singh <gagandeep.singh@safetyculture.com>
2019-04-14 13:13:29 +1000

    A lot of code cleanup (#15)

---
+++ NRLPlayerStats/Stats/MatchStats/View/MatchStatsViewController.swift
@@ -0,0 +1,136 @@
+//
+//  MatchStatsViewController.swift
+//  NRLPlayerStats
+//
+//  Created by Gagandeep Singh on 13/4/19.
+//  Copyright © 2019 Gagandeep Singh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MatchStatsViewController: StatsViewController<[LeagueStat]> {
+    var match: MatchSelectionViewController.Match = .init() {
+        didSet {
+            let endpoint: StatsEndpoint = .match(match.year, match.round, match.game)
+            statsLoader = .init(endpoint: endpoint)
+        }
+    }
+    
+    var viewState: ViewState = .init() {
+        didSet {
+            title = viewState.title
+            tableView.reloadData()
+        }
+    }
+    
+    override func loadView() {
+        super.loadView()
+        title = "Match Stats"
+        registerCells(MatchStatsPlayerCell.self)
+        registerForPreviewing(with: self, sourceView: tableView)
+    }
+    
+    override func didFinishLoading(stat: [LeagueStat]) {
+        viewState = .init(stats: stat)
+    }
+    
+    override func showFailureAlert() {
+        let message = "No stats found for this match 😞"
+        showFailureAlert(message: message) { [weak self] in
+            self?.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    private func createPlayerStatsViewController(teamId: Int, playerId: Int) -> UIViewController {
+        let playerStatsViewController = PlayerStatsViewController()
+        playerStatsViewController.details = (teamId, playerId)
+        return playerStatsViewController
+    }
+    
+    private func presentPlayerStats(teamId: Int, playerId: Int) {
+        let viewController = createPlayerStatsViewController(teamId: teamId, playerId: playerId)
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension MatchStatsViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return viewState.numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewState.numberOfRows(in: section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: MatchStatsPlayerCell.reuseIdentifier, for: indexPath)
+        if let statCell = cell as? MatchStatsPlayerCell {
+            statCell.viewState = viewState.cellViewState(at: indexPath)
+            statCell.delegate = self
+        }
+        
+        cell.selectionStyle = .none
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension MatchStatsViewController {
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return viewState.headerHeight
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return viewState.titleForSection(at: section)
+    }
+}
+
+// MARK: - MatchStatsPlayerCellDelegate
+
+extension MatchStatsViewController: MatchStatsPlayerCellDelegate {
+    func matchStatsPlayerCell(_ cell: MatchStatsPlayerCell, didTapOnHeadshot playerId: Int, teamId: Int) {
+        presentPlayerStats(teamId: teamId, playerId: playerId)
+    }
+}
+
+// MARK: - UIViewControllerPreviewingDelegate
+
+extension MatchStatsViewController: UIViewControllerPreviewingDelegate {
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        guard let (preview, source) = previewViewController(at: location) else { return nil }
+        previewingContext.sourceRect = source
+        return preview
+    }
+    
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
+        navigationController?.pushViewController(viewControllerToCommit, animated: false)
+    }
+    
+    // Its a bit of work getting peek & pop working here but its definitely worth it.
+    func previewViewController(at location: CGPoint) -> (UIViewController, CGRect)? {
+        guard
+            let indexPath = tableView.indexPathForRow(at: location),
+            let cell = tableView.cellForRow(at: indexPath)
+        else { return nil }
+        
+        let mid = UIScreen.main.bounds.width / 2
+        let isInLeftView = location.x <= mid
+        
+        let players = viewState.players(at: indexPath)
+        let teams = viewState.teamsIds(at: indexPath)
+        let team = isInLeftView ? teams.0 : teams.1
+        let player = isInLeftView ? players.0 : players.1
+        
+        guard let teamId = team, let playerId = player?.id else { return nil }
+        
+        let viewController = createPlayerStatsViewController(teamId: teamId, playerId: playerId)
+        var viewFrame = cell.frame
+        viewFrame.origin.x = isInLeftView ? 0 : mid
+        viewFrame.size.width = mid
+        return (viewController, viewFrame)
+    }
+}
---
+++ NRLPlayerStats/Stats/StatsViewController/StatsViewController.swift
@@ -0,0 +1,71 @@
+//
+//  StatsViewControllerProtocol.swift
+//  NRLPlayerStats
+//
+//  Created by Gagandeep Singh on 14/4/19.
+//  Copyright © 2019 Gagandeep Singh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StatsViewController<T: Decodable>: UITableViewController {
+    var statsLoader: StatsLoader<T>?
+    
+    func registerCells(_ cells: NibLoading.Type...) {
+        cells.forEach { tableView.registerCell($0) }
+    }
+    
+    override func loadView() {
+        super.loadView()
+        setupRefreshControl()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadStats()
+    }
+    
+    func setupRefreshControl() {
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(loadStats), for: .valueChanged)
+    }
+    
+    // MARK: - Load Stats
+    
+    @objc
+    func loadStats() {
+        statsLoader?.request { [weak self] result in
+            self?.refreshControl?.endRefreshing()
+            
+            switch result {
+            case .success(let stat):
+                self?.didFinishLoading(stat: stat)
+            default:
+                self?.showFailureAlert()
+            }
+        }
+    }
+    
+    // MARK: - Handle Load Completion
+    
+    // There has to be a better way of doing this, but with Generic Types,
+    // things don't work like you would think they do, yet.
+    
+    /// Subclasses must override this methods to handle completion
+    func didFinishLoading(stat: T) {}
+    
+    /// Subclasses must override this methods to handle failure
+    func showFailureAlert() {}
+    
+    // MARK: - Show Failure Alert
+    
+    func showFailureAlert(message: String, completion: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .cancel) { _ in
+            completion?()
+        }
+        alert.addAction(ok)
+        present(alert, animated: true)
+    }
+}
